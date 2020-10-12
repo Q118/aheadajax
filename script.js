223e5396cd84503cbe387d4d118e92e5396acafc
Q118 <shelbyfish91@gmail.com>
2020-10-12 16:34:32 -0700

    numbers with commas applied

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ function findMatches(wordToMatch, cities) {
     })
 }
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+  
+
 function displayMatches() {
     const matchArray = findMatches(this.value, cities);
     const html = matchArray.map(place => {
@@ -26,8 +31,8 @@ function displayMatches() {
         const stateName = place.state.replace(regex, `<span class = "hl">${this.value}</span>`);
         return `
             <li> 
-            <span class = "name">${cityName}, ${place.state}</span>
-            <span class = "population">${place.population}</span>
+            <span class = "name">${cityName}, ${stateName}</span>
+            <span class = "population">${numberWithCommas(place.population)}</span>
             </li>
         `;
     });
