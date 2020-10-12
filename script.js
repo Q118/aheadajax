0517ea30226c16452979978b9472245abd607c2f
Q118 <shelbyfish91@gmail.com>
2020-10-12 15:59:25 -0700

    pushing data into the cities array

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 const cities = [];
 
 // fetch will return a promise
-fetch(endpoint).then(blob);
+fetch(endpoint)
+    .then(blob => blob.json())
+    //spread into this push method to five us proper array 
+    .then(data => cities.push(...data));
